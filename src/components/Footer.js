a1735201d211761a90cfb8db4e36d8c25528c2e9
mohamedammar269 <mohamed.ammar.kmnews@gmail.com>
2026-05-16 21:10:21 +0100

    Fix conneion button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -105,7 +105,7 @@ export default function Footer() {
             réservés.
           </span>
           <Link href="/admin" className="hover:text-gold transition-colors">
-            Administration
+            Connexion
           </Link>
         </div>
       </div>
