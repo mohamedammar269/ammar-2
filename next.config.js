25ea5385c9eacaeb5feca6d3c0954573c71ca7d0
mohamedammar269 <mohamed.ammar.kmnews@gmail.com>
2026-05-16 21:43:51 +0100

    Fix Facebook bot 403 - allow crawlers

--- next.config.js
+++ next.config.js
@@ -1,9 +1,33 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       { protocol: 'https', hostname: '**' },
+//     ],
+//   },
+// }
+// module.exports = nextConfig
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: '**' },
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'index, follow',
+          },
+        ],
+      },
+    ]
   },
 }
 module.exports = nextConfig
